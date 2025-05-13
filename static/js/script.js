c844d9cd3f345a9cc1568069363e3bff384e9299
Alexej-Mich61 <spas-expert@yandex.ru>
2025-05-13 18:37:40 +0300

    11 полноэкранный режим для камер

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,9 @@
+console.log('script.js loaded'); // Диагностика загрузки
+
 $(document).ready(function() {
+    console.log('jQuery ready'); // Диагностика jQuery
+
+    // Фильтрация районов
     $('#id_region').change(function() {
         var regionId = $(this).val();
         if (regionId) {
@@ -13,5 +18,27 @@ $(document).ready(function() {
         } else {
             $('#id_district').empty().append('<option value="">Выберите район</option>');
         }
+    });
+
+    // Полноэкранный режим для камер
+    $(document).on('click', '.fullscreen-btn', function() {
+        console.log('Fullscreen button clicked'); // Отладка
+        var cameraId = $(this).data('camera-id');
+        console.log('Camera ID:', cameraId); // Отладка
+        var streamUrl = '/cameras/cameras/' + cameraId + '/stream/';
+        console.log('Stream URL:', streamUrl); // Отладка
+        $('#fullscreenStream').attr('src', streamUrl);
+        try {
+            $('#fullscreenModal').modal('show');
+            console.log('Modal should be shown'); // Отладка
+        } catch (e) {
+            console.error('Error opening modal:', e); // Диагностика ошибок Bootstrap
+        }
+    });
+
+    // Очистка потока при закрытии модального окна
+    $('#fullscreenModal').on('hidden.bs.modal', function() {
+        console.log('Modal closed'); // Отладка
+        $('#fullscreenStream').attr('src', '');
     });
 });
